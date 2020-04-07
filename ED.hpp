08040f976dc1a50ce531b7f17d9ffc69ec1d7150
adamtremmm <ajtrembl@gmail.com>
2020-04-07 14:13:51 -0400

    Created ED.hpp API file. Not completely finished. Just started making the ED class.

---
+++ ED.hpp
@@ -0,0 +1,23 @@
+#include <string>
+
+class ED {
+ public:
+	// accepts the two strings to be compared
+	ED (std::string s1, std::string s2);
+
+	// returns the penalty for aligning chars a and b (this will be a 0 or a 1)
+	static int penalty (char a, char b);
+
+	// returns the min of the three args
+	static int min (int a, int b, int c);
+
+	// populates the matrix based on having the two strings, 
+	// and returns the optimal distance (from the [0][0] cell 
+	// of the matrix when done).
+	int optDistance ();
+
+	// traces the matrix and returns a string that can be
+	// printed to display the actual alignment. In general, 
+	// this will be a multi-line string
+	std::string alignment();
+};
